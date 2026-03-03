d86d98ce25ec3bb70d6e6ab013e52e9e0f5d2fda
bysalojee-boop <bysalojee@gmail.com>
2026-03-03 14:44:55 +0200

    Delete prisma directory

--- prisma/migrations/20260227162100_add_seasons_and_soft_delete/migration.sql
+++
@@ -1,4 +0,0 @@
--- This migration is a no-op for PostgreSQL.
--- All tables and columns were consolidated into the 20260227101624_init migration.
--- Originally contained SQLite-specific PRAGMA statements that are not compatible with PostgreSQL.
-SELECT 1;
